fbacdb336eaf96366341831e9f423e23e34125ac
Ivan <krasikov.ivan3010@gmail.com>
2024-05-14 20:19:49 +0300

    Second commit

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
-if 1 == 1:
-    #pass
+if 1 == 2:
+    pass
     #TOKEN = "6414239502:AAFZUBMYaVJWAArBIFEwJTBKDYIRRuveDAc"
     #iam_token = "t1.9euelZqUlceemZrInZbHkMyXyZTOnu3rnpWajs3Hx8vJzc2Wns-TlsyQzZzl8_dOYHdN-e9YAXYI_t3z9w4PdU3571gBdgj-zef1656VmpScm5DHks_Hlo3MjJCQnoyW7_zF656VmpScm5DHks_Hlo3MjJCQnoyWveuelZqckYnIzZeUl5rPmJyOns2Ky7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.imcrntnBDZsWLouzRPhzYWP3wM_yI1jrE46wnaP6fmieZVCESksMGmjBF-JaeSUWS5uCCI68W-3FuFqKrpcwBA"
     #folder_id = "b1g6pphdkkmmv2c4j28c"
-    HOME_DIR = 'C:/Users/user/PycharmProjects/pythonProject/pythonProject'
-    LOGS = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/log_file.txt'  # файл для логов
-    srcogg = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/audio.ogg'
-    DB_FILE = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/speech_kit3.db'  # файл для базы данных
-    IAM_TOKEN_PATH = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/creds/iam_token.txt'  # файл для хранения iam_token
-    FOLDER_ID_PATH = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/creds/folder_id.txt'  # файл для хранения folder_id
-    BOT_TOKEN_PATH = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/creds/bot_token.txt'  # файл для хранения bot_token
+    # HOME_DIR = 'C:/Users/user/PycharmProjects/pythonProject/pythonProject'
+    # LOGS = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/log_file.txt'  # файл для логов
+    # srcogg = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/audio.ogg'
+    # DB_FILE = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/speech_kit3.db'  # файл для базы данных
+    # IAM_TOKEN_PATH = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/creds/iam_token.txt'  # файл для хранения iam_token
+    # FOLDER_ID_PATH = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/creds/folder_id.txt'  # файл для хранения folder_id
+    # BOT_TOKEN_PATH = 'C:/Users/user/PycharmProjects/pythonProject/ФинальныйФинал1/creds/bot_token.txt'  # файл для хранения bot_token
 else:
-    pass
-    # HOME_DIR = '/home/student' #/pythonProject'  # путь к папке с проектом
-    # LOGS = f'{HOME_DIR}/log_file.txt'  # файл для логов
-    # DB_FILE = f'{HOME_DIR}/speech_kit3.db'  # файл для базы данных
-    # srcogg = f'{HOME_DIR}/audio.ogg'
-    # IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'  # файл для хранения iam_token
-    # FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'  # файл для хранения folder_id
-    # BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'  # файл для хранения bot_token
+    #pass
+    HOME_DIR = '/home/student' #/pythonProject'  # путь к папке с проектом
+    LOGS = f'{HOME_DIR}/log_file.txt'  # файл для логов
+    DB_FILE = f'{HOME_DIR}/speech_kit3.db'  # файл для базы данных
+    srcogg = f'{HOME_DIR}/audio.ogg'
+    IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'  # файл для хранения iam_token
+    FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'  # файл для хранения folder_id
+    BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'  # файл для хранения bot_token
